69c80444d270d6e7e0638b9c597316c6185efe68
jayskeez <jkin2520@students.vsu.edu>
2024-11-28 08:46:07 -0500

    Add dump method to Node class for tree output

--- src/Node.java
+++ src/Node.java
@@ -4,12 +4,15 @@ public class Node {
     public int data;
     private static int counter = 0;
 
+    // Constructor
     public Node(int data) {
         this.data = data;
         this.nodeID = counter++;
         this.left = null;
         this.right = null;
     }
+
+    // Insert Method
     public void insert(int value) {
         if (value < this.data) {
             if (this.left == null) {
@@ -23,7 +26,23 @@ public class Node {
             } else {
                 this.right.insert(value);
             }
+            
         }
+    
+        
     }
+    // Dump method
+    public String dump(int level) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t".repeat(level))
+          .append("Node ID: ").append(nodeID)
+          .append(", Data: ").append(data).append("\n");
+        if (this.left != null) sb.append(this.left.dump(level + 1));
+        if (this.right != null) sb.append(this.right.dump(level + 1));
+        return sb.toString();
+    }
+    
+    
+    
     
 }
